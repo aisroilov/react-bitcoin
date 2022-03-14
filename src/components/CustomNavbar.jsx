0733928466aae86741bafa1deaf7a94aef6a1a3e
aisroilov <50108113+aisroilov@users.noreply.github.com>
2022-03-14 13:44:38 +0100

    Menu

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -1,33 +1,24 @@
 import React from "react";
 import { Navbar, Container, Offcanvas, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import "./CustomNavbar.scss";
 
 const CustomNavbar = () => {
   return (
     <>
-      <Navbar bg="dark" expand={false} variant="dark">
-        <Container fluid>
-          <Navbar.Brand href="#">Probearbeit Frontend</Navbar.Brand>
-          <Navbar.Toggle aria-controls="offcanvasNavbar" />
-          <Navbar.Offcanvas
-            id="offcanvasNavbar"
-            aria-labelledby="offcanvasNavbarLabel"
-            placement="end"
-          >
-            <Offcanvas.Header closeButton>
-              <Offcanvas.Title id="offcanvasNavbarLabel">Menu</Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-              <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Link className="nav-link" to="/">
-                  Home
-                </Link>
-                <Nav.Link href="#action2">Link</Nav.Link>
-              </Nav>
-            </Offcanvas.Body>
-          </Navbar.Offcanvas>
-        </Container>
+      <Navbar expand="lg" variant="dark" bg="dark" fixed="top">
+        <Link className="nav-link nav-brand" to="/">
+          Probearbeit Frontend
+        </Link>
       </Navbar>
+      <div class="sidebar">
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
+        <Link className="nav-link" to="/task">
+          Task
+        </Link>
+      </div>
     </>
   );
 };
